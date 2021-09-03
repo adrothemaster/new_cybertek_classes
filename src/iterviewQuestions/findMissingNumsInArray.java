db5ebe61d544e8cfcc3e9f6270b7453f02d04bb1
adrothemaster <81396222+adrothemaster@users.noreply.github.com>
2021-09-03 13:59:19 -0500

    new push for my new project

---
+++ src/iterviewQuestions/findMissingNumsInArray.java
@@ -0,0 +1,13 @@
+package iterviewQuestions;
+
+public class findMissingNumsInArray {
+    public static void main(String[] args) {
+
+        int[] arr1={7,5,6,1,4,2};
+        //Missing numner : 3
+
+
+
+
+    }
+}
--- src/iterviewQuestions/smallestToLargestArray.java
+++ src/iterviewQuestions/smallestToLargestArray.java
@@ -2,7 +2,8 @@ package iterviewQuestions;
 
 public class smallestToLargestArray {
     public static void main(String[] args) {
-        int[] arr = new int[]{5, 10, 15, 20, 25, 30, 1};
+        int[] arr = new int[]{-100000,-3, 655, -11, 5, 10, 15, 20, -25, 30, 1};
+
 
         int max = arr[0];
         int min = arr[0];
@@ -15,5 +16,28 @@ public class smallestToLargestArray {
         }
         System.out.println("min = " + min);
         System.out.println("max = " + max);
+
+    }
+
+    /**
+     * figure out later how to return BELOW!!!!
+     * @param a
+     * @return
+     */
+    public static int minOrMax(int a) {
+        int[] arr = {-100000, -3, 655, -11, 5, 10, 15, 20, -25, 30, 1};
+        int max = Integer.MAX_VALUE;
+        int min = Integer.MIN_VALUE;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+            if (arr[i] < min) {
+                min = arr[i];
+
+            }
+        }
+        return a;
     }
 }
