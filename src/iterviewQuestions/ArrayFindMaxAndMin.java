d434060083f92e219df7994344fd3bbbf7599bd6
adrothemaster <81396222+adrothemaster@users.noreply.github.com>
2021-09-04 22:53:59 -0500

    interview questions practice

--- src/iterviewQuestions/ArrayFindMaxAndMin.java
+++ src/iterviewQuestions/ArrayFindMaxAndMin.java
@@ -1,10 +1,14 @@
 package iterviewQuestions;
 
+import java.util.Arrays;
+
 public class ArrayFindMaxAndMin {
     public static void main(String[] args) {
         int[] example = {100,20000,300,400,1,2,5987,4,3,8,7,55,};
         System.out.println("MaxValue in example = " + MaxValue(example));
         System.out.println("MinValue of example = " + MinValue(example));
+        System.out.println("minValue example with Arrays.sort() = " + minValue(example));
+        System.out.println("maxValue example with Arrays.sort() = " + maxValue(example));
 
     }
     public static int MaxValue(int[] n){
@@ -20,5 +24,22 @@ public class ArrayFindMaxAndMin {
             if (each<min)
                 min = each;
             return min;
+    }
+    public static int minValue( int[]  n ) {
+
+        Arrays.sort( n );
+        System.out.println(Arrays.toString(n));
+
+
+
+        return  n [ 0 ];
+
+    }
+    public static int maxValue(int[] n) {
+        Arrays.sort(n);
+        System.out.println(Arrays.toString(n));
+
+
+        return n[n.length - 1];
     }
 }
