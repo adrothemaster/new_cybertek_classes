e16ae265b315b33bda61b041e008d9a76cdae6ad
adrothemaster <81396222+adrothemaster@users.noreply.github.com>
2021-09-06 21:18:28 -0500

    interview questions practice Order was added here

--- src/day54_abstraction/Order.java
+++ src/day54_abstraction/Order.java
@@ -3,7 +3,13 @@ package day54_abstraction;
 public class Order {
     public static void main(String[] args) {
         Pizza pizza = new Pizza();
+        pizza.order();
         pizza.prepare();
         pizza.serve();
+
+        Salad salad = new Salad();
+        salad.order();
+        salad.prepare();
+        salad.serve();
     }
 }
