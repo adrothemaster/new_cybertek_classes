b69ea42f3adcc0133ed606833d292c09882e3484
adrothemaster <81396222+adrothemaster@users.noreply.github.com>
2021-09-11 17:40:12 -0500

    interview questions practice office hours tasks

--- src/OfficeHoursMyExamples/HidePassword.java
+++ src/OfficeHoursMyExamples/HidePassword.java
@@ -1,11 +1,31 @@
 package OfficeHoursMyExamples;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class HidePassword {
     public static void main(String[] args) {
 
         String [] password = {"one", "apple", "hold"};
-        ArrayList<String> hiddenPassword = new ArrayList<>();
+        System.out.println("Arrays.toString(password) = " + Arrays.toString(password));
+        ArrayList<String> answer = new ArrayList<>();
+
+
+
+
+
+        for (int i = 0; i < password.length; i++) {
+
+            int temp = password[i].length();
+            String hidder = "";
+
+            for (int j = 0; j < temp; j++) {
+                hidder += "*";
+
+            }
+            answer.add(hidder);
+        }
+        System.out.println("answer = " + answer);
+
     }
 }
