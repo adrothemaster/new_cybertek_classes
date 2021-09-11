23e7bc90c3e0be6bbe575218a0e53156c7547f7b
adrothemaster <81396222+adrothemaster@users.noreply.github.com>
2021-09-11 18:16:31 -0500

    interview questions practice office hours tasks

---
+++ src/OfficeHoursMyExamples/TargetWord.java
@@ -0,0 +1,15 @@
+package OfficeHoursMyExamples;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class TargetWord{
+    public static void main(String[] args) {
+
+        ArrayList<String> words = new ArrayList<>(Arrays.asList("java", "html", "javascript", "java", "word" ));
+        String targetWord = "java";
+
+        int count = 0;
+
+    }
+}
