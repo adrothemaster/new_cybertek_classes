5bc2b1127619a26a220e386e2cb924de52a194c3
adrothemaster <81396222+adrothemaster@users.noreply.github.com>
2021-09-11 17:13:11 -0500

    interview questions practice office hours tasks

--- src/OfficeHoursMyExamples/CountLetters.java
+++ src/OfficeHoursMyExamples/CountLetters.java
@@ -6,21 +6,20 @@ import java.util.Arrays;
 public class CountLetters {
     public static void main(String[] args) {
         ArrayList<String> words = new ArrayList<>(Arrays.asList("java", "html", "javascript", "java", "word"));
-        char targetLetter = 'j';
+        char targetLetter = 'a';
+        int total = 0;
 
         for (String each: words) {
-            System.out.println("lettersWithinWord each, targetLetter = " + (lettersWithinWord(each, targetLetter)));
-
+            System.out.println("lettersWithinWord(each,targetLetter) = " + lettersWithinWord(each, targetLetter));
+            total += lettersWithinWord(each, targetLetter);
         }
-
-        System.out.println("lettersWithinWord(\"java\" , \"j\") = " + lettersWithinWord("java", 'j'));
-
-
+        System.out.println(total);
 
     }
 
     public static int lettersWithinWord(String word, char target) {
-        System.out.println("word = " + word + "target: " + target);
+
+
         int count = 0;
 
         for (int i = 0; i < word.length(); i++) {
@@ -28,13 +27,11 @@ public class CountLetters {
         char temp = word.charAt(i);
 
         if (temp == target) {
-                System.out.println("inside if statement");
+
                 count++;
 
             }
         }
         return count;
-
-
     }
 }
