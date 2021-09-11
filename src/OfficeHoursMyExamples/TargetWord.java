3e389875d5acb61542d990a49469d90a099faea1
adrothemaster <81396222+adrothemaster@users.noreply.github.com>
2021-09-11 18:25:17 -0500

    interview questions practice office hours tasks

--- src/OfficeHoursMyExamples/TargetWord.java
+++ src/OfficeHoursMyExamples/TargetWord.java
@@ -7,9 +7,18 @@ public class TargetWord{
     public static void main(String[] args) {
 
         ArrayList<String> words = new ArrayList<>(Arrays.asList("java", "html", "javascript", "java", "word" ));
-        String targetWord = "java";
+        String targetWord = "word";
+        int count=0;
 
-        int count = 0;
+        for (int i = 0; i < words.size(); i++) {
 
+            System.out.println("words.get(i) = " + words.get(i));
+            if(words.get(i).equals(targetWord)){
+                count++;
+                System.out.println("count = " + count);
+            }
+
+        }
+        System.out.println(targetWord + " was found " + count + " times\n");
     }
 }
